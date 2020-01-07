e090750d3d522c559b9c014dff6bdda15b23fa49
victorq <victorqrsilva@gmail.com>
2020-01-06 21:43:26 -0300

    Add direction rotation

--- lib/direction.ex
+++ lib/direction.ex
@@ -22,4 +22,83 @@ defmodule Direction do
   def is_direction(value) do
     value == :north or value == :east or value == :south or value == :west
   end
+
+  @doc """
+  Rotates 90 degrees to the left or right.
+
+  ## Parameters
+
+    - current_direction: direction to be rotated from
+
+  ## Examples
+
+      iex> Direction.rotate(:left, :north)
+      :west
+
+      iex> Direction.rotate(:right, :north)
+      :east
+
+      iex> Direction.rotate_left(nil)
+      nil
+
+  """
+  def rotate(command, current_direction) do
+    case command do
+      :left -> rotate_left(current_direction)
+      :right -> rotate_right(current_direction)
+      _ -> nil
+    end
+  end
+
+  @doc """
+  Rotates 90 degrees to the left.
+
+  ## Parameters
+
+    - current_direction: direction to be rotated from
+
+  ## Examples
+
+      iex> Direction.rotate_left(:north)
+      :west
+
+      iex> Direction.rotate_left(nil)
+      nil
+
+  """
+  def rotate_left(current_direction) do
+    case current_direction do
+      :north -> :west
+      :south -> :east
+      :west -> :south
+      :east -> :north
+      _ -> nil
+    end
+  end
+
+  @doc """
+  Rotates 90 degrees to the right.
+
+  ## Parameters
+
+    - current_direction: direction to be rotated from
+
+  ## Examples
+
+      iex> Direction.rotate_right(:north)
+      :east
+
+      iex> Direction.rotate_right(nil)
+      nil
+
+  """
+  def rotate_right(current_direction) do
+    case current_direction do
+      :north -> :east
+      :south -> :west
+      :west -> :north
+      :east -> :south
+      _ -> nil
+    end
+  end
 end
--- test/direction_test.exs
+++ test/direction_test.exs
@@ -2,6 +2,7 @@ defmodule DirectionTest do
   use ExUnit.Case
   doctest Direction
 
+  # Check if it is a direction
   test ":north is a direction" do
     assert Direction.is_direction(:north)
   end
@@ -24,5 +25,58 @@ defmodule DirectionTest do
 
   test "nil is not a direction" do
     refute Direction.is_direction(nil)
+  end
+
+  # Rotate right
+  test "rotating right from north gives east" do
+    assert Direction.rotate_right(:north) == :east
+  end
+
+  test "rotating right from east gives south" do
+    assert Direction.rotate_right(:east) == :south
+  end
+
+  test "rotating right from south gives west" do
+    assert Direction.rotate_right(:south) == :west
+  end
+
+  test "rotating right from west gives north" do
+    assert Direction.rotate_right(:west) == :north
+  end
+
+  test "rotating right from a non-direction gives nil" do
+    assert Direction.rotate_right(nil) == nil
+  end
+
+  # Rotate left
+  test "rotating left from north gives west" do
+    assert Direction.rotate_left(:north) == :west
+  end
+
+  test "rotating left from west gives south" do
+    assert Direction.rotate_left(:west) == :south
+  end
+
+  test "rotating left from south gives east" do
+    assert Direction.rotate_left(:south) == :east
+  end
+
+  test "rotating left from east gives north" do
+    assert Direction.rotate_left(:east) == :north
+  end
+
+  test "rotating left from a non-direction gives nil" do
+    assert Direction.rotate_left(nil) == nil
+  end
+
+  # rotating
+  test "rotating left" do
+    assert Direction.rotate(:left, :north) == :west
+  end
+  test "rotating right" do
+    assert Direction.rotate(:right, :north) == :east
+  end
+  test "rotating non-direction" do
+    assert Direction.rotate(nil, :north) == nil
   end
 end
